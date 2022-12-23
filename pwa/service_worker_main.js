35ef4e177cd30e53dfd7b052e3cd3f830aa90b7e
The Doggy Brad <94173621+thedoggybrad@users.noreply.github.com>
2022-12-23 20:00:27 +0800

    Done with PWA

--- pwa/service_worker_main.js
+++ pwa/service_worker_main.js
@@ -1,1 +1,30 @@
+'use strict'
 
+const CACHE_NAME = 'simplepasswordgenerator';
+// The files we want to cache
+const resourceList = [
+  '/',
+  'https://thedoggybrad.github.io/simplepasswordgenerator/index.html',
+  'https://thedoggybrad.github.io/simplepasswordgenerator/421648.png',
+  'https://thedoggybrad.github.io/simplepasswordgenerator/script.js',
+  'https://thedoggybrad.github.io/simplepasswordgenerator/style.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => {
+    return cache.addAll(resourceList);
+  }));
+});
+
+function addToCache(cacheName, resourceList) {
+  caches.open(cacheName).then(cache => {
+    return cache.addAll(resourceList);
+  });
+}
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => {
+    return response || fetch(event.request);
+  }));
+});
